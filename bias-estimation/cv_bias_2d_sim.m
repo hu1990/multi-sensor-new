76949832d52193f3e12d15ad4c6eab37f0b92d1c
hu <huzhihenghit@gmail.com>
2019-07-13 21:23:18 +0800

    bias estimate complete

--- bias-estimation/cv_bias_2d_sim.m
+++ bias-estimation/cv_bias_2d_sim.m
@@ -1,11 +1,4 @@
-% This is an EKF demo.
-% 2D scenario
-% Use two bearing-only sensors to track a constant velocity target.
-% Filter model:
-%   system model:
-%     CV model + constant sensor bias
-%   measurement model:
-%     two bearing-only sensors + constant bias
+function sim_data = cv_bias_2d_sim(smart_sensor)
 rng(0);
 % Simulation setting
 dt = 1;
@@ -13,13 +6,13 @@ total_time = 20;
 iters = total_time/dt;
 
 % Define targets
-target.pos = [0, 200000]';
-target.vel = [5, -4200]';
+target.pos = [200000, 0]';
+target.vel = [-4200, 5]';
 target.pos_history = [target.pos];
 
 % Define sensors
 sensor1.pos = [0, 0]';
-sensor1.vel = [0, 3000]';
+sensor1.vel = [3000, 0]';
 sensor1.pos_history = [sensor1.pos];
 sensor1.bias = 10/60/57.3;
 sensor1.std = 1e-5;
@@ -27,37 +20,37 @@ sensor1.std = 1e-5;
 % This relative position can give a better result.
 % sensor2.pos = [500, 0]';
 % While this cannot.
-sensor2.pos = [5000, 0]';
-sensor2.vel = [0, 3000]';
+sensor2.pos = [0, 5000]';
+sensor2.vel = [3000, 0]';
 sensor2.pos_history = [sensor2.pos];
 sensor2.bias = -15/60/57.3;
 sensor2.std = 1e-5;
 
 % Define filter
-otsekf.x = [0, 0, 199000, -6000, 0, 0]';
-otsekf.P = diag([10, 10, 1e3, 1200, 1e-2, 1e-2])^2;
+otsekf.x = [199000, 0, -6000, 0, 0, 0]';
+otsekf.P = diag([1e3, 10, 1200, 10, 1e-2, 1e-2])^2;
 
 otsekf.f = @fx;
 
 otsekf.F = @F;
 
 q = 1e-5;
-Qx = [dt^3/3 dt^2/2 0 0;
-	dt^2/2 dt 0 0;
-	0 0 dt^3/3 dt^2/2;
-	0 0 dt^2/2 dt]*q;
+Qx = [dt^3/3 0 dt^2/2 0;
+	dt^2/2 0 dt 0;
+	0 dt^3/3 0 dt^2/2;
+	0 dt^2/2 0 dt]*q;
 Qb = diag([1e-6, 1e-6])^2;
 otsekf.Q = [Qx zeros(4,2);
     zeros(2,4) Qb];
 
 otsekf.h = @two_bearing_sensor2d_hx;
 
-
 otsekf.H = @two_bearing_sensor2d_H;
 
 otsekf.R = diag([1e-5, 1e-5])^2;
 
-otsekf.result.b =[otsekf.x(5:6)];
+otsekf.result.x = [otsekf.x(1:4)];
+otsekf.result.b = [otsekf.x(5:6)];
 
 % helper attributes
 otsekf.dim_x = size(otsekf.x, 1);
@@ -70,6 +63,9 @@ for iter = 1:iters
     
     % Sensor motion
     sensor1 = cv_motion(sensor1, dt);
+    if smart_sensor
+        sensor2.vel = optimal_vel(target, sensor1, sensor2);
+    end
     sensor2 = cv_motion(sensor2, dt);
     
     % Generate measurement
@@ -87,23 +83,25 @@ time = 0:dt:dt*iters;
 
 % Plot
 % Target trajectory
-figure
-plot(time, target.pos_history(2,:));
+% figure
+% plot(time, target.pos_history(2,:));
 
-figure;
-hold on
-plot([time(1), time(end)], [sensor1.bias, sensor1.bias]);
-plot(time, otsekf.result.b(1,:));
-legend('bias1', 'estimate1');
+% Return simulation result
+sim_data.time = time;
+sim_data.target_pos = target.pos_history;
+sim_data.sensor1_pos = sensor1.pos_history;
+sim_data.sensor2_pos = sensor2.pos_history;
+sim_data.x = otsekf.result.x;
+sim_data.b = otsekf.result.b;
+sim_data.sensor1_bias = sensor1.bias;
+sim_data.sensor2_bias = sensor2.bias;
+sim_data.target_vel = target.vel;
+end
 
-figure;
-hold on
-plot([time(1), time(end)], [sensor2.bias, sensor2.bias]);
-plot(time, otsekf.result.b(2,:));
 function f = F(x, dt)
     f = eye(6);
-    f(1,2) = dt;
-    f(3,4) = dt;
+    f(1,3) = dt;
+    f(2,4) = dt;
 end
 function f = fx(x, dt)
     f = F(x, dt) * x;
@@ -111,22 +109,22 @@ end
 function h = two_bearing_sensor2d_hx(x, args)
     sensor1_pos = args.sensor1_pos;
     sensor2_pos = args.sensor2_pos;
-    target_pos = x(1:2:3);
+    target_pos = x(1:2);
     x1 = target_pos - sensor1_pos;
     x2 = target_pos - sensor2_pos;
-    h = [atan2(x1(1), x1(2)) + x(5);
-        atan2(x2(1), x2(2)) + x(6)];
+    h = [atan2(x1(2), x1(1)) + x(5);
+        atan2(x2(2), x2(1)) + x(6)];
 end
 function H = two_bearing_sensor2d_H(x, args)
     sensor1_pos = args.sensor1_pos;
     sensor2_pos = args.sensor2_pos;
-    target_pos = x(1:2:3);
+    target_pos = x(1:2);
     x1 = target_pos - sensor1_pos;
     mag1 = norm(x1)^2;
     x2 = target_pos - sensor2_pos;
     mag2 = norm(x2)^2;
-    H = [x1(2)/mag1 0 -x1(1)/mag1 0 1 0;
-        x2(2)/mag2 0 -x2(1)/mag2 0 0 1];
+    H = [-x1(2)/mag1 x1(1)/mag1 0 0 1 0;
+        -x2(2)/mag2 x2(1)/mag2 0 0 0 1];
 end
 function target = cv_motion(target, dt)
     target.pos = target.pos + dt * target.vel;
@@ -135,5 +133,5 @@ end
 
 function obs = bearing_measure(sensor, target)
     x = target.pos - sensor.pos;
-    obs = atan2(x(1), x(2)) + sensor.bias + sensor.std *randn;
+    obs = atan2(x(2), x(1)) + sensor.bias + sensor.std *randn;
 end
---
+++ bias-estimation/readme.md
@@ -0,0 +1,52 @@
+Here are something about sensor bias estimation.
+
+`smart_sensor_simulation.m`: 
+
+Simulation scenario is as follows.
+
+Two bearing-only sensors cooperate to track a constant velocity target.
+The sensors are moving in a constant speed, too, while:
+- in Simulation 1, Sensor 2 moves as same as Sensor 1;
+- in Simulation 2, Sensor 2 decides an optimal heading angle.
+
+Filter method used is EKF actually.
+
+Simulation results are plotted.
+
+`fim_max.mlx`:
+
+A realtime script showing how to solve the optimal heading angle.
+
+`ekf_predict_update.m`:
+
+An implement of EKF.
+
+`test_asekf.m`:
+
+An EKF demo.
+
+2D scenario.
+Use two bearing-only sensors to track a constant velocity target.
+
+Filter model:
+  - system model:
+    CV model + constant sensor bias
+  - measurement model:
+    two bearing-only sensors + constant bias
+
+`otsekf_predict_update.m`:
+
+An implement of OTSEKF (optimal two stage extended Kalman filter).
+*There exists a linearization approximation.*
+
+`test_otsekf.m`:
+
+An OTSEKF demo.
+
+The simulation scenario is as same as in `test_asekf.m`.
+
+`EM.m`:
+
+Used for inplement EM method for linear system identification.
+This example is from `An explanation of the Expectation Maximization Algorithm`.
+The MATLAB code in the paper and the relavent toolbox are referenced.
